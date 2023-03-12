c82ac4e5dd6beae2128d69bf79df29a0f90e7dca
Roge55555 <roge55555@gmail.com>
2023-03-13 00:54:05 +0300

    feat: add smooth transitions (Mon, Mar 13, 2023 00:54:05)

--- cssBayan/index.js
+++ cssBayan/index.js
@@ -3,7 +3,6 @@ const listElm = document.querySelectorAll(".memeElement");
 listElm.forEach(chosenElm => {
     chosenElm.addEventListener('click', function() {
         document.querySelector(".chosen")?.classList.remove("chosen");
-        console.log(chosenElm.children[1].classList)
-        chosenElm.children[1].classList.add("chosen");
+        chosenElm.classList.add("chosen");
     });
 });
